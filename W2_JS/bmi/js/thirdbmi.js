2d53277e10d7917bfe5ce8de8958570eafe0c3bb
K Lowrie <lowriekate@gmail.com>
2016-01-26 10:56:29 -0500

    JS and DOM
    Some new, some cleaned up and reorganized

---
+++ W2_JS/bmi/js/thirdbmi.js
@@ -0,0 +1,14 @@
+function calculateBMI(){
+	var weight = Number(document.getElementById("weight").value);
+	var feet   = Number(document.getElementById("feet").value);
+	var inches = Number(document.getElementById("inches").value);
+	var height = feet * 12 + inches;
+
+	bmi = weight / (height * height) * 703;
+
+	var result=document.getElementById("result");
+	result.innerHTML = 'With height ' + height + ' and weight ' + weight +
+						' your BMI is: ' + bmi;
+
+	return false;
+}
