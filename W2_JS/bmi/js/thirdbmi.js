8c3d9dc189e47b9dcc29bd3ae7c277cd7afa0dd9
lowriek <lowriek@bc.edu>
2016-02-02 12:52:30 -0500

    Cleanup

--- W2_JS/bmi/js/thirdbmi.js
+++ W2_JS/bmi/js/thirdbmi.js
@@ -5,10 +5,24 @@ function calculateBMI(){
 	var height = feet * 12 + inches;
 
 	var bmi = weight / (height * height) * 703;
+	
+	var message;
+	var img;
+	if (bmi >= 30 ){
+		img = '<br><img src="img/obese.gif" alt="obese" />';
+		message = ' you are obese';
+	} else if (bmi >= 25 ) {
+		message = ' you are overweight';
+	} else if (bmi >= 18.5) {
+		message = ' you are normal weight';
+	} else {
+		message = ' you are underweight';
+	}
+	
 
 	var result=document.getElementById("result");
 	result.innerHTML = 'With height ' + height + ' and weight ' + weight +
-						' your BMI is: ' + bmi;
+						' your BMI is: ' + bmi + message + img;
 
 	return false;
 }
