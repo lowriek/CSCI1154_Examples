6d9b9bb39fd9786b18c946c312d24477fbfbd06f
lowriek <lowriek@bc.edu>
2016-02-05 08:34:56 -0500

    updated examples
    Added Loops

---
+++ W3_Loops/js/loop.js
@@ -0,0 +1,13 @@
+document.getElementById("myBtn").addEventListener("click", myHandler);
+function myHandler(){
+		
+    // get the count
+    var mynum = Number(document.getElementById('mynumber').value);
+    var outputstr;
+			
+			
+    // output the list
+    document.getElementById('myoutput').innerHTML = outputstr;
+}
+
+
