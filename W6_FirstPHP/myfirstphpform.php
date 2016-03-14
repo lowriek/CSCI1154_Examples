54a0104128142a3582cf7b8d8e6ad9bf41710e39
K Lowrie <lowriekate@gmail.com>
2016-03-14 15:29:46 -0400

    Cleanup
    Added labels and CSS for alignment, made display_form() a function.
    Updated coding style.

--- W6_FirstPHP/myfirstphpform.php
+++ W6_FirstPHP/myfirstphpform.php
@@ -2,38 +2,40 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>My First PHP Form</title>
+	<title>PHP Form Example</title>
+	<link rel="stylesheet" type="text/css" href="css/base.css" />
 </head>
 <body>
-<?php  
-	echo "<pre>";
-	print_r($_GET);
-	echo "</pre>"; 
-?>
-<fieldset>
-	<legend>The First Form</legend>
-	<form method="get">
-		First: <input type="text" name="count1"/><br><br>
-		Second: <input type="text" name="count2"/>
-		<input type="submit"  name="mysubmit" value="Go"/>
-	</form>
-</fieldset>
-<br>
-<br>
+<?php  echo "<pre>"; print_r( $_GET ); echo "</pre>"; ?>
 <?php 
-	if (isset($_GET['mysubmit'])) {
-		handleform();
+	display_form();
+	if ( isset( $_GET['mysubmit'] ) ) {
+		handle_form();
 	}
 ?>
 </body>
 </html>
 <?php
-function handleform(){
+function display_form(){
+?>
+<fieldset>
+	<legend>The First Form</legend>
+	<form method="get">
+		<label for="count1">First: </label><input type="text" name="count1"/><br>
+		<label for="count2">Second: </label><input type="text" name="count2"/>
+		<input type="submit"  name="mysubmit" value="Go"/>
+	</form>
+</fieldset>
+<?php
+}
+ 
+function handle_form(){
     $a = $_GET['count1'];
     $b = $_GET['count2'];
     $sum = $a + $b;
     echo "  <fieldset><legend>The results</legend>
-                The sum is $sum
+                The sum is $sum.
             </fieldset>";
 }
-?>
+
+
